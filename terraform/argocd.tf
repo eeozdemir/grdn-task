21a88e4e5996789e359801bbd02cc2424ed3fa5b
Emre Özdemir <ozdemirree@gmail.com>
2024-09-15 11:32:04 +0300

    add terraform configurations and kubernetes manifests

---
+++ terraform/argocd.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  depends_on = [module.eks]
+}
